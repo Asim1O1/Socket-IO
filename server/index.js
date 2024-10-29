7fb8a2372c55eb4690beaca6fa64e27671580447
Asim1O1 <146157042+Asim1O1@users.noreply.github.com>
2024-10-29 14:11:28 +0545

    backend socket io

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-import express from "express";
-import { Server } from "socket.io";
-import { createServer } from "http";
+// import express from "express";
+// import { Server } from "socket.io";
+// import { createServer } from "http";
 
-const app = express();
-const server = createServer(app);
+// const app = express();
+// const server = createServer(app);
 
-const io = new Server(server);
+// const io = new Server(server);
 
-io.on("connection", (socket) => {
-  console.log("connection established");
-});
-app.get("/", (req, res) => {
-  res.send("Creating a chat app!");
-});
+// io.on("connection", (socket) => {
+//   console.log("connection established");
+// });
+// app.get("/", (req, res) => {
+//   res.send("Creating a chat app!");
+// });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
